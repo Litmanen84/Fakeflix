d820ec1661d64bba250c5d4decc47c32c63e474a
Pietro Fusco <pietrofusco84@gmail.com>
2024-04-24 09:49:15 +0200

    ciao

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-refresh';
 // import assets from 'vite-plugin-assets'; 
 
 export default defineConfig({
-  base: '/',
+  base: '/Fakeflix/',
   plugins: [
     react(),
     // assets({
